479b8d5dd1a78a423d6abfefd176896a8b7c5dbb
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-08 00:41:46 +0300

    asteroids: Implement single tap & UDFPS sensors
    Also remove double tap to wake as touchscreen firmware does not
    support it.
    
    Change-Id: If32201674515f0f8d1592af36eb7e3668eea898a

--- device.mk
+++ device.mk
@@ -543,8 +543,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sysconfig_wfc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/sysconfig_wfc.xml
 
 # Power
-$(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/gesture_mode)
-
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
@@ -603,6 +601,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
+    sensors.asteroids \
     sensors.dynamic_sensor_hal
 
 PRODUCT_VENDOR_PROPERTIES += \
