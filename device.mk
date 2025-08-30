ced5380c6cb304c4a2e4aad39457006e712b13a8
Alexander Koskovich <akoskovich@pm.me>
2025-08-30 00:30:42 -0400

    asteroids: Drop libagmmixer from PRODUCT_PACKAGES
    Target no longer exists.
    
    Change-Id: I8e53d1a24a14eece7c5ea407ac9c2710b130ec03

--- device.mk
+++ device.mk
@@ -98,7 +98,6 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
-    libagmmixer \
     libaudiochargerlistener \
     libbatterylistener \
     libcustomva_intf \
