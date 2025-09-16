6a448ea2c4b7bbfa72f4a570ec7976910ad65922
Alexander Koskovich <akoskovich@pm.me>
2025-09-16 18:32:01 -0400

    asteroids: Fix VINTF VTS failures
    Remove unused interfaces:
      vendor.qti.hardware.factory
      vendor.qti.hardware.wifi.wifilearner
    
    Copy bluetooth_audio.xml from stock instead as our actual version is v3,
    the version of the file we are copying from AOSP is v4.
    
    Remove android.hardware.bluetooth.audio-impl from PRODUCT_PACKAGES as it
    is not actually used, we use android.hardware.bluetooth.audio-impl-qti.
    
    Change-Id: I65d8316fe129bdf105eee4071120b2d9795b744f

--- device.mk
+++ device.mk
@@ -85,7 +85,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
     audio.primary.volcano \
