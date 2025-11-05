007a37716665a4a68ee295ecf303d1370fce7c15
sayann70 <sayansarkarda2@gmail.com>
2025-11-05 23:16:18 +0530

    asteroids: Move props into configs folder
    For easier management:
    - odm.prop -> configs/props/odm.prop
    - product.prop -> configs/props/product.prop
    - system_ext.prop -> configs/props/system_ext.prop
    - vendor.prop -> configs/props/vendor.prop
    
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -207,10 +207,10 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := volcano
 
 # Properties 
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
