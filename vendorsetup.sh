0fd35e57ae2081c7aab5553257d6e4708a9ac438
sayann70 <sayansarkarda2@gmail.com>
2025-11-18 23:08:40 +0530

    asteroids: Setup `vendorsetup.sh`
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# Kernel
+git clone https://github.com/NullDebris/android_kernel_nothing_sm7635 -b 6.6/lineage-23.0 kernel/nothing/sm7635
+git clone https://github.com/NullDebris/android_kernel_nothing_sm7635-devicetrees -b lineage-23.0 kernel/nothing/sm7635-devicetrees
+git clone https://github.com/NullDebris/android_kernel_nothing_sm7635-modules -b lineage-23.0 kernel/nothing/sm7635-modules
+
+# Vendor
+git clone https://github.com/NullDebris/proprietary_vendor_nothing_asteroids -b lineage-23.0 vendor/nothing/asteroids
+
+# Dolby
+git clone https://github.com/NullDebris/hardware_dolby -b sony hardware/dolby
+
+# Glyph-Interface
+git clone https://github.com/NullDebris/packages_apps_ParanoidGlyph -b lineage-23.0 packages/apps/ParanoidGlyph
+git clone https://github.com/NullDebris/packages_apps_GlyphAdapter -b 15 packages/apps/GlyphAdapter
